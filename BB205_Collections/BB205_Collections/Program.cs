7991d959820a2f14afc570ea31409fbca83ccf6f
RasulRustamli <rasular@code.edu.az>
2023-10-24 16:56:37 +0400

    feat:added delegate

---
+++ BB205_Collections/BB205_Collections/Program.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+
+namespace BB205_Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ArrayList
+            //ArrayList arrayList = new ArrayList();
+
+            //arrayList.Add("Hello");
+            //arrayList.Add("Hello1");
+            //arrayList.Add("Hello2");
+            //arrayList.Add("Hello3");
+            //arrayList.Add("Hello4");
+            //arrayList.Add("Hello5");
+            //arrayList.Add("Hello6");
+            //arrayList.Add("Hello7");
+            //arrayList.Add("Hello8");
+
+
+
+            //foreach (var i in arrayList)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            ////arrayList.Sort();
+            //Console.WriteLine("-------------------");
+
+
+            ////Console.WriteLine(arrayList.Capacity);
+
+            ////Console.WriteLine(arrayList.Count);
+            //arrayList.RemoveRange(3,1);
+            //foreach (var i in arrayList)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+
+            #region Queue
+
+            //Queue queue = new Queue();
+            //queue.Enqueue("salam");
+            //queue.Enqueue(1);
+            //queue.Enqueue("Hello");
+            //queue.Enqueue(1.5);
+
+            //foreach (var item in queue)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //queue.Dequeue();
+
+            //Console.WriteLine("------------------");
+            //foreach (var item in queue)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Console.WriteLine("sirada olan " +queue.Peek());
+            //Console.WriteLine(queue.Peek());
+
+            #endregion
+            #region Stack
+
+            //Stack stack = new Stack();
+            //stack.Push("salam");
+            //stack.Push("sagol");
+            //stack.Push(3);
+
+            //foreach (var item in stack)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("siradaki object " +stack.Peek());
+            //stack.Pop();
+            //Console.WriteLine("siradaki object " + stack.Peek());
+
+
+            #endregion
+
+            #region HashTable
+            //Hashtable ht = new Hashtable();
+            //ht.Add("sagol", "hello");
+
+            //ht.Add(1, "world");
+            //ht.Add("hello", "world");
+            //foreach (var key in ht.Values)
+            //{
+            //    Console.WriteLine(key);
+            //}
+
+
+            #endregion
+
+            #region SortedList
+
+            SortedList sortedList = new SortedList();
+
+            sortedList.Add(2, "salam");
+            sortedList.Add(1, 3);
+            sortedList.Add(3, 12);
+
+            foreach (var i in sortedList.Values)
+            {
+                Console.WriteLine(i);
+            }
+            #endregion
+        }
+    }
+}
---
+++ BB205_Delegate/BB205_Delegate/Program.cs
@@ -0,0 +1,139 @@
+﻿namespace BB205_Delegate
+{
+
+
+    //internal delegate bool CheckNum(int item);
+    //internal delegate void PrintString<in T>(T str);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int[] arr = {1,2,3,4, 5, 6, 7};
+
+            //////int result=Sum(arr,CheckPositive);
+            //////Console.WriteLine(result);
+
+
+            //////Capitalize(word);
+
+            //////print(word);
+            //////print += PrintToUpper;
+            //////print += Capitalize;
+            //////print -= PrintToLower;
+            ////string word = "saLAm";
+            //////PrintString print= delegate (string item)
+            //////{
+            //////    Console.WriteLine(item[0]);
+            //////};
+            //////print += PrintToUpper;
+
+            ////PrintString<string> print = PrintToLower;
+
+
+            ////print += (item) => Console.WriteLine(item);
+
+
+            ////CheckNum check=n=>n % 3 == 0;
+
+            //Action<string> str = PrintToLower;
+            //int result=Sum(arr, n => n  > 0);
+
+
+            
+            //Console.WriteLine(result);
+
+
+
+            List<string> list = new List<string>();
+
+            list.Add("salam");
+            list.Add("Hello");
+            list.Add("BB205");
+
+            //list.ForEach(s => Console.WriteLine(s));
+
+            list.FindAll(s => s.Contains("l")).ForEach(s => Console.WriteLine(s));
+
+        }
+
+        public static void PrintToLower(string word)
+        {
+            Console.WriteLine(word.ToLower());
+        }
+        public static void PrintToUpper(string word)
+        {
+            Console.WriteLine(word.ToUpper());
+        }
+        public static void Capitalize(string word)
+        {
+            Console.WriteLine(Char.ToUpper(word[0])+word.Substring(1).ToLower());
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public static int Sum(int[] arr,Func<int,bool> a)
+        {
+            
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (a(arr[i]))
+                {
+                    sum += arr[i];
+                }
+            }
+            return sum;
+        }
+
+
+
+       
+
+
+
+
+
+
+       public  static bool CheckPositive(int num)
+        {
+            return num > 0;
+        }
+        public static bool CheckOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+        public static bool CheckEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+    }
+}
